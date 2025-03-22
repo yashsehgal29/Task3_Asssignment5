203357fc7e56597adcef6bea617b915198f800e6
yashsehgal29 <yashsehgal2974@gmail.com>
2025-03-22 15:27:37 +0530

    first commit

---
+++ todo.py
@@ -0,0 +1,16 @@
+tasks = []
+
+def add_task(task):
+    tasks.append(task)
+    print(f'Task "{task}" added.')
+
+def list_tasks():
+    if not tasks:
+        print("No tasks available.")
+    else:
+        for idx, task in enumerate(tasks, start=1):
+            print(f'{idx}. {task}')
+
+if __name__ == "__main__":
+    add_task("Finish Assignment")
+    list_tasks()
